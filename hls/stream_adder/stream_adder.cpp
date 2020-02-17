13333a90b02232e5629eff958a4de5373a66bb46
otenim <snake_istobelieve@keio.jp>
2020-02-17 23:41:21 +0900

    update

--- hls/stream_adder/stream_adder.cpp
+++ hls/stream_adder/stream_adder.cpp
@@ -7,13 +7,16 @@ void stream_adder(istream* input, ostream* output, ap_uint<DATALEN/2> reg) {
 #pragma HLS interface s_axilite register port=reg
 #pragma HLS interface ap_ctrl_none port=return
 
-	ap_uint<DATALEN/2> a, b;
+	ap_uint<DATALEN/2> a, b, c;
 
 	// extract data
-	a = input->data.range(DATALEN - 1, DATALEN / 2);
-	b = input->data.range(DATALEN / 2 - 1, 0);
+	a = input->data.range(DATALEN - 1, DATALEN / 2); // upper 32 bits
+	b = input->data.range(DATALEN / 2 - 1, 0); // lower 32 bit
 
-	// create output
-	output->data = (a + b) * reg;
+	// add & multiply
+	c = (a + b) * reg;
+
+	// create output data
+	output->data = c;
 	output->last = input->last;
 }
