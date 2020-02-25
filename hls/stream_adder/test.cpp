7112c3f40cea0164b464c031a4beff08a08d0cb8
otenim <snake_istobelieve@keio.jp>
2020-02-25 20:55:23 +0900

    update

--- hls/stream_adder/test.cpp
+++ hls/stream_adder/test.cpp
@@ -2,42 +2,34 @@
 #include <ap_int.h>
 #include <stdlib.h>
 #include <math.h>
-#define MAXIMUM 1000
+
+#define REG 5
 #define N 10
 
-void create_istream(istream* input, ap_uint<DATALEN> data) {
-	input->data = data;
-	input->last = 0;
-}
 
 int main(int argc, char** argv) {
-	ap_uint<DATALEN> data;
-	ap_uint<DATALEN/2> a, b, reg;
+	ap_uint<DATALEN/2> a, b;
+	ap_uint<DATALEN/2> reg = REG;
 	istream input;
 	ostream output;
 
 	for (int i = 0; i < N; i++) {
-		a = rand() % MAXIMUM; // 0 ~ (MAXIMUM - 1)
-		b = rand() % MAXIMUM;
-		reg = rand() % MAXIMUM;
-		data = (a, b);
-		create_istream(&input, data);
+		a = i;
+		b = i;
+
+		// create input
+		input.data = (a, b); // concatination
+		input.last = 0;
+
+		// compute
 		stream_adder(&input, &output, reg);
+
+		// print output
 		printf("[%d]:\n", i);
-		printf("\t a = %s(%s)\n",
-				a.to_string(16).c_str(),
-				a.to_string(10).c_str());
-		printf("\t b = %s(%s)\n",
-				b.to_string(16).c_str(),
-				b.to_string(10).c_str());
-		printf("\t reg = %s(%s)\n",
-				reg.to_string(16).c_str(),
-				reg.to_string(10).c_str());
-		printf("\t [input] data = %s\n",
-				data.to_string(16).c_str());
-		printf("\t [output] data = %s(%s)\n",
-				output.data.to_string(16).c_str(),
-				output.data.to_string(10).c_str());
+		printf("upper 32-bit field: %s\n", a.to_string(10).c_str());
+		printf("lower 32-bit field: %s\n", b.to_string(10).c_str());
+		printf("reg: %s\n", reg.to_string(10).c_str());
+		printf("output: %s\n", output.data.to_string(10).c_str());
 	}
 
 	return EXIT_SUCCESS;
